dc62556a00e2f6c0f7d34278feac6f8ab46265aa
franek86 <frane.konjevoda@gmail.com>
2026-02-19 10:24:57 +0100

    fix gitignore and dockerignore

--- dist/schemas/notification.schema.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UpdateNotificationSchema = void 0;
-const zod_1 = require("zod");
-exports.UpdateNotificationSchema = zod_1.z.object({
-    isRead: zod_1.z.boolean(),
-});
