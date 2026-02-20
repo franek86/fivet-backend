fe1bd0ad0c584616a1960f33718a227a0b4d8455
franek86 <frane.konjevoda@gmail.com>
2026-02-20 12:25:18 +0100

    fix setCookies adn deploy app

--- src/utils/cookies/setCookies.ts
+++ src/utils/cookies/setCookies.ts
@@ -1,10 +1,11 @@
 import { Response } from "express";
 
 export const setCookie = (res: Response, name: string, value: string, maxAge: number) => {
+  const isProduction = process.env.NODE_ENV === "production";
   res.cookie(name, value, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict", // TO DO: in production sameSite must be true
+    secure: isProduction,
+    sameSite: isProduction ? "none" : "strict", // TO DO: in production sameSite must be true
     maxAge,
   });
 };
