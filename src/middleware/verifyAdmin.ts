063c891c279f9fc9245831b384cd22ca4ff038b3
franek86 <frane.konjevoda@gmail.com>
2026-03-18 13:58:07 +0100

    Fix bug in verify admin

--- src/middleware/verifyAdmin.ts
+++ src/middleware/verifyAdmin.ts
@@ -8,15 +8,37 @@ export interface CustomJwtPayload {
 }
 
 const authAdmin = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  let token: string | undefined;
+  // Check Authorization header
+  const authHeader = req.headers.authorization;
+  if (authHeader && authHeader.startsWith("Bearer ")) {
+    token = authHeader.split(" ")[1];
+  }
+
+  if (!token && req.cookies?.access_token) {
+    token = req.cookies.access_token;
+  }
+
+  if (!token) {
+    res.status(401).json({ message: "Unauthorized" });
+    return;
+  }
+
   if (!req.user || req.user.role !== "ADMIN") {
     res.status(403).json({ message: "Access denied. Admins only." });
   }
-  const token = req.cookies.access_token;
-  if (!token) res.status(401).json({ message: "Unauthorized" });
-  const decoded = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
-  req.user = decoded;
 
-  next();
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
+    if (!decoded) {
+      res.status(401).json({ message: "Unauthorized! Invalid token" });
+      return;
+    }
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Unauthorized. Token expired or invalid." });
+  }
 };
 
 export default authAdmin;
